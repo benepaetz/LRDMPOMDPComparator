7cb56ccdd30a0def3fe9e2c936798fcb6d2fd495
benepaetz <benedikt.paetzold@mailbox.tu-dresden.de>
2026-04-13 12:28:47 +0200

    Delete

--- src/org/lrdm/topologies/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package containing topology strategies of the simulator. Currently supported topologies are: {@link org.lrdm.topologies.FullyConnectedTopology}, {@link org.lrdm.topologies.NConnectedTopology} and {@link org.lrdm.topologies.BalancedTreeTopologyStrategy}.
- */
-package org.lrdm.topologies;
